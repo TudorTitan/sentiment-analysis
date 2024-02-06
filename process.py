8fd50f5e56d10e2b9b81a3e134a740c512e567cf
Tudor Ciurca <33178381+TudorTitan@users.noreply.github.com>
2024-02-06 14:28:05 +0000

    Update process.py
    Removed optional functions

--- process.py
+++ process.py
@@ -8,8 +8,6 @@ import yfinance as yf
 
 
 Vectors = get_database()
-articles = Vectors["table"]
-validation_articles = Vectors["validation_table"]
 
 #compute how many days ago a unix timestamp is
 def days_ago(timestamp):
@@ -28,48 +26,21 @@ def compute_weekly_performance(unix_date):
 
 #compute performance metrics associated to articles in the articles database
 def update_classes():
+    articles = Vectors['Assets']
     table = articles.find()
     for item in list(table):
         result = compute_weekly_performance(item['date'])
-        articles.update_one({'_id': item['_id']},{'$set': { 'class': result  } })
+        articles.update_one({'_id': item['_id']},{'$set': { 'label': result  } })
 
 def predict(article, k, candidates):
+    articles = Vectors['Assets']
     # total measure of articles published when asset moves at least 1% that week
-    positive_articles = articles.count_documents({'class': {'$gt': 0.01}})
+    positive_articles = articles.count_documents({'label': {'$gt': 0.01}})
     negative_articles = articles.count_documents({}) - positive_articles
     weight_neg = negative_articles / (negative_articles + positive_articles)
     weight_pos = positive_articles / (negative_articles + positive_articles)
     neighbours = query(get_embedding(article), k, candidates)
-    good_neighbours = sum([1 for a in list(neighbours) if a['class'] > 0.01])
+    good_neighbours = sum([1 for a in list(neighbours) if a['label'] > 0.01])
     # we need to weight the sample accoridng to total articles
     probability = (good_neighbours / weight_pos) / (good_neighbours / weight_pos + (k - good_neighbours) / weight_neg)
     return probability
-
-#check performance of articles by using validation_articles
-#k nearest neighbours, candidates is typically 20*k
-def validate(k,candidates):
-    #total measure of articles published when asset moves at least 1% that week
-    positive_articles = articles.count_documents({'class':{'$gt':0.01}})
-    negative_articles = articles.count_documents({}) - positive_articles
-    weight_neg = negative_articles/ (negative_articles + positive_articles)
-    weight_pos = positive_articles/ (negative_articles + positive_articles)
-    table = validation_articles.find()
-    errors = 0
-    total = 0
-    print('The proportion of articles influencing BTC to move >1% is ' + str(weight_pos))
-    for item in table:
-        neighbours = query(item['vector'],k,candidates)
-        good_neighbours = sum([1 for a in list(neighbours) if a['class'] > 0.01])
-        #we need to weight the sample accoridng to total articles
-        probability = (good_neighbours / weight_pos) / (good_neighbours / weight_pos + (k - good_neighbours) / weight_neg)
-        #only execute if sure
-        if probability < 0.8:
-            continue
-        total += 1
-        #count errors, if BTC actually moves negatively
-        if item['class'] < 0:
-            errors += 1
-        print('I am ' + str(probability) + ' sure that BTC will go up, but the actual performance is ' + str(item['class']))
-        print('cumulative error: ' + str(errors/total))
-
-#validate(24,500)
